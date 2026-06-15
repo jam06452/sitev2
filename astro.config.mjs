518092652e552960b527e0848d3550ea41721285
q <jam@jam06452.uk>
2026-06-15 15:26:43 +0100

    More SEO, robots, sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://jam06452.uk",
@@ -11,6 +13,6 @@ export default defineConfig({
       noExternal: ["lucide-react"],
     },
   },
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   output: "static",
 })
